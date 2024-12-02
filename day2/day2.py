5e38fe6e82f2367bbcb1ed53ad5c0fb8ecba8a19
Jou Xiong <j.xiong@fetchrewards.com>
2024-12-02 16:29:26 -0600

    🎉 day2 🎉  dnoe

--- day2/day2.py
+++ day2/day2.py
@@ -15,24 +15,25 @@ def make_list():
 def find_safe_reports(reports_list):
     safe_reports_count = 0
     for report in reports_list:
-        safe = False
+        rule1_safe = True
+        for i in range(len(report)-1):
+            # check difference
+            difference = abs(report[i] - report[i+1])
+            if difference > 3 or difference < 1:
+                rule1_safe = False
+                break
+        # check increase/decrease
         is_decreasing = False
         is_increasing = False
-        for i in report:
-            # check difference
-            difference = report[i] - report[i+1]
-            if difference < 0:
-                difference = difference*-1
-            if difference > 3 or difference < 1:
-                break
-
-
-
-        diff = list1[i] - list2[i]
-        if diff < 0:
-            diff = diff*-1
-        difference += diff
-    return difference
+        for i in range(len(report) - 1):
+            if report[i] < report[i + 1]:
+                is_increasing = True
+            elif report[i] > report[i + 1]:
+                is_decreasing = True
+        rule2_safe = not (is_increasing and is_decreasing)
+        if rule1_safe and rule2_safe:
+            safe_reports_count += 1
+    return safe_reports_count
 
 # for part 2
 # def find_similarity_between_lists(list1, list2):
@@ -45,8 +46,8 @@ def find_safe_reports(reports_list):
 
 def main():
     reports_list = make_list()
-    difference = find_safe_reports(list1, list2)
-    print(f'The distance between both lists is {difference}')
+    safe_reports_count = find_safe_reports(reports_list)
+    print(f'The number of safe reports is {safe_reports_count}')
     # similarity = find_similarity_between_lists(list1, list2)
     # print(f'The similarity between both lists is {similarity}')
 
